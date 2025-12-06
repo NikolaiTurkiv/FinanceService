6f9f4eefb9733be1c287bd188769cf94474f9293
nikolaj <nikolai.cebb@gmail.com>
2025-12-06 10:59:43 -0800

    Test homework

---
+++ src/test/java/MIFITest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MIFITest {
+    @Test
+    void testStringEquality() {
+        String expected = "MIFI_GITHUB_ACTIONS_HOME_WORK";
+        String actual = "MIFI_GITHUB_ACTIONS_HOME_WORK";
+
+        assertEquals(expected, actual, "Строки должны совпадать");
+    }
+}
